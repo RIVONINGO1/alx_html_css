ee8f1dc298b336ee107317dba788ee64d357ff53
RIVONINGO1 <99362228+RIVONINGO1@users.noreply.github.com>
2025-06-01 20:51:06 +0200

    Added 7-multi_c.js script to print 'C is fun' x times

---
+++ Desktop/7-multi_c.js
@@ -0,0 +1,10 @@
+const args = process.argv.slice(2);
+
+if (args.length === 0 || isNaN(parseInt(args[0]))) {
+  console.log("Missing number of occurrences");
+} else {
+  const x = parseInt(args[0]);
+  for (let i = 0; i < x; i++) {
+    console.log("C is fun");
+  }
+}
